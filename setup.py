49b53220a06a6d444bdc81aea40d1bd201178f6a
Kim Ebert <kim@indicio.tech>
2025-05-17 14:59:38 -0600

    chore: use indicio indy plenum
    Signed-off-by: Kim Ebert <kim@indicio.tech>

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
         (BASE_DIR, ['data/nssm_original.exe'])
     )],
 
-    install_requires=['indy-plenum==1.14.0-rc0',
+    install_requires=['indicio-indy-plenum==1.13.2-rc12',
                     # importlib-metadata needs to be pinned to 3.10.1 because from v4.0.0 the package
                     # name ends in python3-importlib-metadata_0.0.0_amd64.deb
                     # see also build-scripts/ubuntu-2004/build-3rd-parties.sh
